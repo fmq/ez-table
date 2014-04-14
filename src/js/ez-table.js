f7fd46b5e386df3db6e7c8584073f6181f798f68
Facundo Miquel <facundoMiquel@gmail.com>
2014-04-14 00:10:57 -0300

    Revert "Cambios menores"
    This reverts commit bb84822b39a961dd41d8b1f0bb1ede556403d025.

--- src/js/ez-table.js
+++ src/js/ez-table.js
@@ -1,4 +1,3 @@
-/*jshint -W043 */
 angular.module('ez.table', [])
 
 .constant('EzTableConfig', {
@@ -111,9 +110,8 @@ angular.module('ez.table', [])
                     
                     scope.pager.setPage = function (pageNum) {
                         console.log(pageNum);
-                        if (pageNum) {
+                        if (pageNum)
                             scope.pager.currentPage = parseInt(pageNum);
-                        }
                         
                         if (scope.pager.maxPages < scope.pager.totalPages) {
                             var startPage = Math.max(scope.pager.currentPage - Math.floor(scope.pager.maxPages/2), 1);                                                           
@@ -138,14 +136,13 @@ angular.module('ez.table', [])
                         }
                         return scope.pager.currentPage;
                         
-                    };
+                    }
                     
                     scope.pager.prev = function () {
-                        if (scope.pager.currentPage > 1) {
+                        if (scope.pager.currentPage > 1)
                             scope.pager.setPage(--scope.pager.currentPage);
-                        }
                         return scope.pager.currentPage;
-                    };
+                    }
                     
                     scope.pager.refresh = function(data) {
                         
@@ -159,12 +156,12 @@ angular.module('ez.table', [])
                         for (startPage; startPage <= endPage ; startPage++) {
                             scope.pager.pages.push(startPage);
                         }
-                    };
+                    }
                     
                     scope.pager.setRowsPerPage = function (rows) {
                         scope.limit = rows;
                         scope.setPage(0);
-                    };
+                    }
                     // END PAGER
                     
                     scope.currentPage = 0;
